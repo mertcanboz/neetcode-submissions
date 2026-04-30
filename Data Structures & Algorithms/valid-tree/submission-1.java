65dc324090495228f74da08a707b53484d1c5d41
Mertcan Boz <52539978+mertcanboz@users.noreply.github.com>
2026-04-30 17:32:51 +0200

    Add: valid-tree - submission-1

---
+++ Data Structures & Algorithms/valid-tree/submission-1.java
@@ -0,0 +1,36 @@
+class Solution {
+    public boolean validTree(int n, int[][] edges) {
+        List<List<Integer>> adj = new ArrayList<>();
+        for (int i=0; i<n; i++) {
+            adj.add(new ArrayList<>());
+        }
+        for (int[] edge: edges) {
+            int from = edge[0];
+            int to   = edge[1];
+            adj.get(from).add(to);
+            adj.get(to).add(from);
+        }
+
+        Set<Integer> seen = new HashSet<>();
+        if (!dfs(0, -1, seen, adj)) {
+            return false;
+        }
+
+        return seen.size() == n;
+    }
+
+    private boolean dfs(int node, int parent, Set<Integer> seen, List<List<Integer>> adj) {
+        if (seen.contains(node))
+            return false;
+
+        seen.add(node);
+        for (int neighbor: adj.get(node)) {
+            if (neighbor == parent) {
+                continue;
+            }
+            if (!dfs(neighbor, node, seen, adj))
+                return false;
+        }
+        return true;
+    }
+}
